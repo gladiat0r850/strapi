e526ef19544b9ea652896b704c191f58ba8dff4d
X3NOGLADIAT0R <game23452332pro@gmail.com>
2024-11-27 22:20:09 +0400

    ez

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   async function checkout(){
-    const res = await fetch('https://strapi-3o8q.onrender.com/create-checkout-session', {
+    const res = await fetch('https://strapi-7-6ydq.onrender.com/create-checkout-session', {
       method: 'POST',
       body: JSON.stringify({items: [1,2]})
     })
